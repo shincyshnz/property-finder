58353301ad0d3d325d9718357404ca9cfd426d68
alpha00-coder <ikdev35@outlook.com>
2024-04-05 12:42:48 +0400

    testing

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ErrorProvider } from "./context/ErrorContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
